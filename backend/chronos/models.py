7a3e477691e2c6cfa39e08815d3dd46c8abe3766
Flamichka <mtjcraft@gmail.com>
2023-10-14 15:02:39 +0300

    added most of requests

--- backend/chronos/models.py
+++ backend/chronos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+    unique_id = models.AutoField(primary_key=True)  
+    name = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    building = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+            return f'Place: {self.name}, Building: {self.building}, Description: {self.description}'
+        
+    class Meta:
+        db_table = 'Places'
+
+class Booking(models.Model):
+    unique_id = models.AutoField(primary_key=True)  
+    full_name = models.CharField(max_length=255, blank=False, null=False)
+    phone_number = models.CharField(max_length=15, blank=False, null=False)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, blank=False, null=False)
+    event_start = models.DateTimeField(blank=False, null=False)
+    event_end = models.DateTimeField(blank=False, null=False)
+    technical_equipment = models.TextField(blank=True, null=True)
+    organizer_info = models.TextField(blank=False, null=False)
+    role = models.IntegerField( blank=False, null=False)
+    is_approved = models.BooleanField(default=False, null=False, blank=False)
+    
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        db_table = 'Bookings'
--- backend/chronos/views.py
+++ backend/chronos/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from .models import Place, Booking
+from .serializers import PlaceSerializer, BookingSerializer, BookingAdminSerializer
 
-# Create your views here.
+class PlaceViewSet(viewsets.ModelViewSet):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+    
+    def list(self, request):
+        serializer = self.serializer_class(self.queryset, many=True)
+        return Response(serializer.data)
+
+class BookingAdminViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingAdminSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CreateBookingViewSet(viewsets.ViewSet):
+    serializer_class = BookingSerializer  # Define the serializer class
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)  # Use self.serializer_class
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
