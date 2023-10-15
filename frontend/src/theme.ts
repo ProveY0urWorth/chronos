e88ec8333ca1115e0efd0b914c97c76c317eaefa
ProveY0urWorth <snowmmodestiny@gmail.com>
2023-10-16 00:09:45 +0300

    Update on hours and form.

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -2,6 +2,7 @@ import { createTheme } from '@mui/material'
 
 const theme = createTheme({
   palette: {
+    mode: 'light',
     background: {
       default: '#FFFFFF',
     },
--- frontend/src/ts/pages/BookingPage/BookingPage.tsx
+++ frontend/src/ts/pages/BookingPage/BookingPage.tsx
@@ -1,9 +1,19 @@
-import { Button, Stack, TextField } from '@mui/material'
-import { Form, Formik } from 'formik'
+import {
+  Button,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  TextareaAutosize,
+  Typography,
+} from '@mui/material'
+import { Field, Form, Formik } from 'formik'
 import React from 'react'
 import styles from './BookingPage.module.scss'
 import classNames from 'classnames/bind'
 import { IBooking } from '../../redux/features/BookingSlice'
+import { LocalizationProvider, TimePicker } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 const cx = classNames.bind(styles)
 
 interface BookingPageProps {
@@ -25,7 +35,7 @@ export const BookingPage: React.FC<BookingPageProps> = ({ className = '' }) => {
   return (
     <Formik
       initialValues={Initial_Values}
-      //validationSchema={CredentialSchema}
+      //validationSchema={BookingSchema}
       onSubmit={(values) => {
         console.log(values)
       }}
@@ -39,7 +49,80 @@ export const BookingPage: React.FC<BookingPageProps> = ({ className = '' }) => {
                 spacing={1}
                 alignItems={'center'}
               >
-                <TextField></TextField>
+                <Typography variant='body2'>ФИО заявителя</Typography>
+                <Field
+                  as={TextField}
+                  name='fullname'
+                  label='Введите ФИО заявителя'
+                  value={values.fullname}
+                  className={cx('credentials__textfield')}
+                />
+                <Typography variant='body2'>Номер телефона</Typography>
+                <Field
+                  as={TextField}
+                  name='phoneNumber'
+                  label='Введите номер телефона'
+                  value={values.phoneNumber}
+                  className={cx('credentials__textfield')}
+                />
+                <Typography variant='body2'>
+                  Техническое оборудование
+                </Typography>
+                <Field
+                  as={TextareaAutosize}
+                  minRows={3}
+                  name='technicalEquipment'
+                  placeholder='Введите перечень технического оборудования'
+                  value={values.technicalEquipment}
+                  className={cx('credentials__textfield')}
+                />
+                <Typography variant='body2'>
+                  Информация о мероприятии
+                </Typography>
+                <Field
+                  as={TextareaAutosize}
+                  minRows={3}
+                  name='organizerInfo'
+                  placeholder='Введите информацию о мероприятии'
+                  value={values.organizerInfo}
+                  className={cx('credentials__textfield')}
+                />
+                <Select
+                  name='role'
+                  label='Роль'
+                  value={values.role}
+                >
+                  <MenuItem
+                    value={0}
+                    disabled
+                  >
+                    Не выбрана
+                  </MenuItem>
+                  <MenuItem value={1}>Студент</MenuItem>
+                  <MenuItem value={2}>Преподаватель</MenuItem>
+                  <MenuItem value={3}>Внешний организатор</MenuItem>
+                </Select>
+                <Select
+                  name='place'
+                  label='Локация'
+                  value={values.place}
+                >
+                  <MenuItem
+                    value={0}
+                    disabled
+                  >
+                    Не выбран
+                  </MenuItem>
+                  <MenuItem value={1}>Хрустальный зал</MenuItem>
+                  <MenuItem value={2}>Зал с хрустальной совой</MenuItem>
+                  <MenuItem value={3}>Хогвартс</MenuItem>
+                </Select>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <TimePicker onChange={(e: any) => console.log(e.$H)} />
+                </LocalizationProvider>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <TimePicker />
+                </LocalizationProvider>
                 <Button
                   onClick={submitForm}
                   className={cx('credentials__button')}
